0f5c2bd9eea440f5c6892eec55a3b54936d93f60
Matthew Canabarro <mec357@cornell.edu>
2022-08-13 22:54:51 -0400

    git 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './script';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
